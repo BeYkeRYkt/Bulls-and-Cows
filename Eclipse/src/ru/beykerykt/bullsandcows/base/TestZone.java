7729cb2723eea363400eb1d69da24a2cc429fe68
BeYkeRYkt <beykerykt@gmail.com>
2017-04-03 00:53:08 +0700

    test: print exceptions ?

--- Eclipse/src/ru/beykerykt/bullsandcows/base/TestZone.java
+++ Eclipse/src/ru/beykerykt/bullsandcows/base/TestZone.java
@@ -8,27 +8,17 @@ import ru.beykerykt.bullsandcows.base.utils.GameUtils;
 
 public class TestZone {
 	public static void main(String[] args) {
-		BasePlayer player = new HumanPlayer("DevelopedOne", GameUtils.generateRandomCode(), new PrintStreamInterface(System.out));
-		BasePlayer bot = new BotPlayer("Bob", GameUtils.generateRandomCode());
+		try {
+			BasePlayer player = new HumanPlayer("DevelopedOne", GameUtils.generateRandomCode(), new PrintStreamInterface(System.out));
+			BasePlayer bot = new BotPlayer("Bob", GameUtils.generateRandomCode());
 
-		BattleArena arena = new BattleArena("Test");
-		arena.addPlayer(player);
-		arena.addPlayer(bot);
-		arena.start();
-
-		// Scanner input = new Scanner(System.in);
-		// player.getUserInterface().showText("Guess a 4-digit number with no duplicate digits: ");
-		// player.getUserInterface().showText(bot.getSecretCode());
-
-		// BobFinder finder = new BobFinder();
-		// finder.reset();
-
-		// while (arena.isRunning()) {
-		// int guess = input.nextInt();
-		// String line = String.valueOf(guess);
-		// player.getUserInterface().showText(bot.getHint(line));
-		// }
-		// input.close();
+			BattleArena arena = new BattleArena("Test");
+			arena.addPlayer(player);
+			arena.addPlayer(bot);
+			arena.start();
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
 	}
 
 }
